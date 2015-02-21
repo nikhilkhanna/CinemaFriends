f8041e95484bed4b0d9870e090f2a717aff3503f
Nikhil Khanna <nikhilkhanna@graphlab.com>
2015-02-21 03:39:22 -0800

    Initial Commit

---
+++ cine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cine
+//
+//  Created by Nikhil Khanna on 2/21/15.
+//  Copyright (c) 2015 TreeHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cine
+//
+//  Created by Nikhil Khanna on 2/21/15.
+//  Copyright (c) 2015 TreeHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
